4c29eb4e55ccf24bb5e4e9ec365e0b345251330d
W-Worach <115943399+W-Worach@users.noreply.github.com>
2026-06-01 19:03:10 +0200

    custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/Wirtualnebiurodimeoffice',
+  base: '/',
   resolve: {
     alias: {
       // Alias @ to the src directory
